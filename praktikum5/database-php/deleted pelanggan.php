e933fe715e7e8c6c61e0428c7c4ef17d9c917e1b
Amirullah18 <amirullah182004@gmail.com>
2023-04-26 10:36:13 +0700

    SQL Database

---
+++ praktikum5/database-php/deleted pelanggan.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("dbkoneksi.php");
+if (isset($_GET["iddelet"])) {
+    $iddelet = $_GET["iddelet"];
+    $query = "DELETE FROM pelanggan WHERE id = '$iddelet'";
+    $delete = $dbh->query($query);
+    echo '<meta http-equiv="refresh" content="0; url=lish_pelanggan.php">' ;
+}
+
+?>
