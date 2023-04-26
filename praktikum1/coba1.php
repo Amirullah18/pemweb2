f08f30f36e1e4496f2fcd46c7b8410364aff11df
Amirullah18 <amirullah182004@gmail.com>
2023-04-26 10:15:48 +0700

    Latihan & Tugas

---
+++ praktikum1/coba1.php
@@ -0,0 +1,9 @@
+<?php
+   define('PHI', 3.14);
+   define('DBNAME','inventori');
+   define('DBNSERVER','localhost');
+
+   echo 'PHI :'.PHI;
+   echo 'Nama database nya : '.DBNAME;
+   echo '<br>Lokasi database nya :' .DBNSERVER;
+?>
---
+++ praktikum1/index.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = 'amirullah';
+    $umur = 18;
+    $berat = 50;
+
+    echo 'Nama Saya :' .$nama;
+    echo '<br>Umur Saya :' .$umur;
+    echo '<br>Berat Saya :' .$berat;
+?>
